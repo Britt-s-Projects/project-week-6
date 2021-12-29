b5153fdd5bd4765a42acb5c38acf26bf191236f8
Brittany <britt.smart31@gmail.com>
2021-12-29 18:26:15 +0000

    add unit conversion

--- src/script.js
+++ src/script.js
@@ -35,16 +35,23 @@ function showTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let iconElement =  document.querySelector("#icon")
   
-  temperatureElement.innerHTML = `${temperature}°C`;
+  temperatureElement.innerHTML = `${temperature}`;
   document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#min").innerHTML = response.data.main.temp_min;
-  document.querySelector("#max").innerHTML = response.data.main.temp_max;
+  document.querySelector("#min").innerHTML = Math.round(response.data.main.temp_min);
+  document.querySelector("#max").innerHTML = Math.round(response.data.main.temp_max);
   document.querySelector("#description").innerHTML = response.data.weather[0].main;
-  document.querySelector("#feels-like").innerHTML = response.data.main.feels_like;
+  document.querySelector("#feels-like").innerHTML = Math.round(response.data.main.feels_like);
   document.querySelector("#humidity").innerHTML =response.data.main.humidity;
   document.querySelector("#wind").innerHTML = response.data.wind.speed;
   iconElement.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+  celsiusTemperature = response.data.main.temp;
+  celsiusMin = response.data.main.temp_min;
+  celsiusMax = response.data.main.temp_max;
+  celsiusFeels = response.data.main.feels_like;
+
 }
+
 function searchCity(city) {
   let apiKey = "4dd89d25d72e4d6c413288eb27dfcc72";
   let apiEndpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
@@ -78,6 +85,34 @@ function getCurrentLocation(event) {
   
 }
 
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+   document.querySelector("#temperature").innerHTML = Math.round(fahrenheitTemperature);
+   let fahrenheitMin = (celsiusMin * 9) / 5 + 32;
+   document.querySelector("#min").innerHTML = Math.round(fahrenheitMin); 
+     let fahrenheitMax = (celsiusMax * 9) / 5 + 32;
+     document.querySelector("#max").innerHTML = Math.round(fahrenheitMax); 
+       let fahrenheitFeels = (celsiusFeels * 9) / 5 + 32;
+       document.querySelector("#feels-like").innerHTML = Math.round(fahrenheitFeels); 
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  document.querySelector("#temperature").innerHTML = Math.round(celsiusTemperature);
+     document.querySelector("#min").innerHTML = Math.round(celsiusMin);
+     document.querySelector("#max").innerHTML = Math.round(celsiusMax);
+     document.querySelector("#feels-like").innerHTML = Math.round(celsiusFeels); 
+}
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
@@ -86,3 +121,7 @@ let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
 searchCity("London");
+
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click",  displayFahrenheitTemperature);
